71dbf5ea3b422ebcb638bcb9815969d4b8baa054
KOZnik <stanczykluk@gmail.com>
2016-03-26 13:37:08 +0100

    Routing added

--- app/mock-heroes.ts
+++ app/mock-heroes.ts
@@ -1,4 +1,5 @@
-import {Hero} from './hero';
+import { Hero } from './hero';
+
 export var HEROES: Hero[] = [
     {"id": 11, "name": "Mr. Nice"},
     {"id": 12, "name": "Narco"},
@@ -11,3 +12,9 @@ export var HEROES: Hero[] = [
     {"id": 19, "name": "Magma"},
     {"id": 20, "name": "Tornado"}
 ];
+
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at http://angular.io/license
+ */
